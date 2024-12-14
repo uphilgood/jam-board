002107a599254710c63b4fe2cba3cea11fbf8fd6
seeck3 <marco.dseo@gmail.com>
2024-12-14 13:51:41 -0500

    added DND board with mock data

--- apps/client/app/boards/page.tsx
+++ apps/client/app/boards/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useAuth } from "../contexts/authContext";
+import { redirect } from "next/navigation";
 
 interface Board {
   id: number;
@@ -128,6 +129,7 @@ export default function BoardPage() {
               {boards.map((board) => (
                 <div
                   key={board.id}
+                  onClick={() => redirect(`boards/${board.id}`)}
                   className="bg-white shadow-md rounded-lg p-4 hover:shadow-xl transition"
                 >
                   <h3 className="text-lg font-semibold mb-2">{board.name}</h3>
