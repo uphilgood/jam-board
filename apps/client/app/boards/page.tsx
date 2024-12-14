9db1899388baafbecf490613f31203828455ca8d
seeck3 <marco.dseo@gmail.com>
2024-12-14 14:01:37 -0500

    fixed meerge conflict

--- apps/client/app/boards/page.tsx
+++ apps/client/app/boards/page.tsx
@@ -3,11 +3,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useAuth } from "../contexts/authContext";
-<<<<<<< HEAD
-import { redirect } from "next/navigation";
-=======
 import { useRouter } from "next/navigation";
->>>>>>> @{-1}
 
 interface Board {
   id: number;
@@ -136,6 +132,7 @@ export default function BoardPage() {
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
               {boards.map((board) => (
                 <div
+                onClick={() => router.push(`/boards/${board.id}`)}
                   key={board.id}
                   className="bg-white shadow-md rounded-lg p-4 hover:shadow-xl transition break-words"
                 >
